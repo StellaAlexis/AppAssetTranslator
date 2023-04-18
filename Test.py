994c54d5b71bfe1ec2e0057ba1bffc8155176fff
Bart <bart@mylermedia.nl>
2023-04-18 09:31:32 +0200

    Add support to generate xml/.strings based on .csv

---
+++ Test.py
@@ -0,0 +1,122 @@
+import yaml
+import pandas as pd
+
+KEY_CONFIG_TRANSLATION_FILE_PATH = "translationFile"
+KEY_CONFIG_SUPPORTED_LANGUAGES = "languages"
+KEY_CONFIG_VARIABLE_NAME = "variableName"
+KEY_CONFIG_LOCALE = "locale"
+KEY_CONFIG_STRINGS_PATH = "stringsPath"
+KEY_CONFIG_XML_PATH = "xmlPath"
+KEY_CONFIG_OUTPUT_PATH = "outputFile"
+
+config = yaml.safe_load(open("config.yaml"))
+dataframe = pd.read_csv(f"{config[KEY_CONFIG_TRANSLATION_FILE_PATH]}", delimiter=";")
+print(dataframe.head())
+print()
+
+
+def create_ios_string_mapping(key, value):
+    return f"\"{key}\" = \"{value}\";"
+
+
+def create_android_string_mapping(key, value):
+    return f"    <string name=\"{key}\">{value}</string>"
+
+
+def remove_none_values(value_list):
+    return [i for i in value_list if i is not None]
+
+
+def get_language_details(language):
+    # This part is required, as the language string, f.e 'Dutch-NL' is 1 layer higher than the required data
+    current_language_key = list(language.keys())[0]
+    language = language[current_language_key]
+
+    string_path = language[KEY_CONFIG_STRINGS_PATH]
+    xml_path = language[KEY_CONFIG_XML_PATH]
+
+    if xml_path is None and string_path is None:
+        pass
+    else:
+        return language
+
+
+def get_languages():
+    language_list = [get_language_details(x) for x in config[KEY_CONFIG_SUPPORTED_LANGUAGES]]
+    return remove_none_values(language_list)
+
+
+def generate_language_resource_files(language):
+    locale = language[KEY_CONFIG_LOCALE]
+    string_path = language[KEY_CONFIG_STRINGS_PATH]
+    xml_path = language[KEY_CONFIG_XML_PATH]
+
+    keys_df = dataframe[config[KEY_CONFIG_VARIABLE_NAME]]
+    # print(keys_df.head())
+
+    if string_path is not None:
+        f = open(string_path, "w")
+
+        results = [create_ios_string_mapping(x, y) for x, y in
+                   zip(dataframe[config[KEY_CONFIG_VARIABLE_NAME]], dataframe[locale])]
+
+        [f.write(f"{x}\n") for x in results]
+
+        f.close()
+
+    if xml_path is not None:
+        f = open(xml_path, "w")
+        f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+        f.write("<resources>\n")
+
+        results = [create_android_string_mapping(x, y) for x, y in
+                   zip(dataframe[config[KEY_CONFIG_VARIABLE_NAME]], dataframe[locale])]
+        [f.write(f"{x}\n") for x in results]
+
+        f.write("</resources>")
+
+
+def generate_dataframe_from_list(data, locale):
+    return pd.DataFrame(data=data, columns=['Key', locale])
+
+
+def generate_csv_from_resource_files(languages):
+    t = [['yes', 'ja'], ['no', 'nein'], ['Greeting', 'Hello']]
+    t2 = [['yes', 'oui'], ['no', 'non']]
+    t3 = [['yes', 'ja'], ['no', 'nee']]
+
+    locales = ['de-DE', 'fr-FR', 'nl-NL']
+
+    language_list = [generate_dataframe_from_list(x, y) for x, y in zip([t, t2, t3], locales)]
+
+    # final_pd = pd.DataFrame(data=['yes', 'no'], columns=['Key'])
+
+    df_1 = pd.DataFrame(data=t, columns=['Key', 'de-DE'])
+    df_2 = pd.DataFrame(data=t2, columns=['Key', 'fr-FR'])
+    df_3 = pd.DataFrame(data=t3, columns=['Key', 'nl-NL'])
+
+    dfs = [df_1, df_2, df_3]
+    [print(f"{x.head()}\n") for x in dfs]
+    # [print(f"{x.head()}\n") for x in language_list]
+
+    final_pd = language_list[0]
+
+    for x in language_list:
+        # final_pd = pd.merge(left=final_pd, right=x, right_on='Key', left_on='Key', how='left')
+        final_pd = pd.merge(left=final_pd, right=x)
+
+    # [pd.merge(left=final_pd, right=x, right_on='Key', left_on='Key', how='left') for x in dfs]
+    # print(f'{final_pd.head()}')
+    #
+    # new_df = pd.merge(left=df_1, right=df_2, right_on="Key", left_on="Key", how="left")
+    # new_df = pd.merge(left=new_df, right=df_3, right_on="Key", left_on="Key", how="left")
+    # print(new_df.head())
+    #
+    # new_df.to_csv(config[KEY_CONFIG_OUTPUT_PATH], index=False, sep=';', encoding='utf-8')
+    print("Generated csv!")
+
+
+result = get_languages()
+[generate_language_resource_files(x) for x in result]
+
+generate_csv_from_resource_files(result)
