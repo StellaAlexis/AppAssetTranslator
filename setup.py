93890b55ec157e1355858d277e626bdef3875345
Stella <4118427+StellaAlexis@users.noreply.github.com>
2024-03-08 23:22:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-import setuptools
+import setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='app_asset_translator',
-    packages=['app_asset_translator'],
+    packages=find_packages(),
     version='0.0.1',
     license='GNU GPLv3',
     description='Filler description',
     long_description=long_description,
     author='Myler Media',
     author_email='developers@mylermedia.nl',
-    # TODO: Change to new public url
-    url='https://gitlab.com/mylermediadevelopers/internal/translation-manager-python',
+    url='https://github.com/StellaAlexis/AppAssetTranslator',
     install_requires=['pandas', 'PyYAML'],
-    # TODO: Update to source code link
-    download_url='url/to/source/code',
+    download_url='https://github.com/StellaAlexis/AppAssetTranslator',
     entry_points='''
         [console_scripts]
         app-asset-translator=app_asset_translator.scripts.translator_scripts:main
